fe1d3bd70236e8dc20cfbc3cc8537be3b9550707
sistemubbl1984 <azaharrabu12@gmail.com>
2025-12-05 09:38:58 +0800

    feat: Add test API endpoint to diagnose deployment

--- api/test.js
+++ api/test.js
@@ -1,4 +1,5 @@
 // api/test.js
-module.exports = (req, res) => {
-  res.status(200).json({ message: 'Hello from the test API! This is a successful test.' });
-};
+export default function handler(req, res) {
+  console.log('--- TEST API DEPLOYMENT SUCCESSFUL ---');
+  res.status(200).json({ message: 'Test API is running successfully.' });
+}
