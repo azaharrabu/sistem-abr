3add699a0320ed2e96cd0a44aaa9b78af1c9f2fc
sistemubbl1984 <azaharrabu12@gmail.com>
2025-12-10 14:02:35 +0800

    fix(leaderboard): Matikan cache untuk endpoint API

--- api/affiliate-leaderboard.js
+++ api/affiliate-leaderboard.js
@@ -22,6 +22,7 @@ module.exports = async (req, res) => {
         }
 
         // Hantar data yang telah diproses sebagai respons
+        res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
         return res.status(200).json(leaderboardData);
 
     } catch (unexpectedError) {
